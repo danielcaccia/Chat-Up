d9d3417039f4f13319cb1e4af325483700c16380
Daniel Caccia <danielcaccia@icloud.com>
2021-02-03 00:15:42 +0100

    Create Views and Segues

---
+++ Chat Up/Controller/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat Up
+//
+//  Created by Daniel Caccia on 02/02/21.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
