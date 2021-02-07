cd080f296db0ad5e966da43dd66406b4163fbfc7
Daniel Caccia <danielcaccia@icloud.com>
2021-02-07 14:13:08 +0100

    Create Responsive UI and Add Color Sets

--- Chat Up/Controller/WelcomeViewController.swift
+++ Chat Up/Controller/WelcomeViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setButtonLayout(signInButton)
+        setButtonLayout(signUpButton)
     }
 
-
+    func setButtonLayout(_ button: UIButton) {
+        button.layer.cornerRadius = 15
+        button.clipsToBounds = true
+    }
+    
 }
 
